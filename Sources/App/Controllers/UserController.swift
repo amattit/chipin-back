7d6b85ea01c2fc83272d7d387d228d6f1f5597ef
MikhailSeregin <mseregin@outlook.com>
2020-01-17 16:10:26 +0300

    fix cString

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -189,9 +189,11 @@ struct IsYandexConnectRequest: Content {
 
 extension Phone {
     func isAwailableFormat() throws -> Bool {
-        let phoneRegExp = "^((9)+([0-9]){9})$"
-        let predecate = NSPredicate(format: "SELF MATCHES %@", phoneRegExp)
-        if predecate.evaluate(with: self) { return predecate.evaluate(with: self)}
-        throw Abort(.badRequest, reason: "Номер телефона в неверном формате")
+        return try "^((9)+([0-9]){9})$".withCString { str -> Bool in
+            let predecate = NSPredicate(format: "SELF MATCHES %@", str)
+            if predecate.evaluate(with: self) { return predecate.evaluate(with: self)}
+            throw Abort(.badRequest, reason: "Номер телефона в неверном формате")
+        }
+        
     }
 }
